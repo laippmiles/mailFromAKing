bfa57a74a116f1141252349301c22da5a384fdbb
laipp123 <laippmiles@gmail.com>
2018-03-23 22:05:11 +0800

    给看到这里的所有少女的礼物

--- main.py
+++ main.py
@@ -15,11 +15,6 @@ def main():
     '''h表示设定的小时，m为设定的分钟'''
     print('Begin')
     h = [8, 14, 23]
-    mailTime = h.copy()
-    index = int(uniform(0, len(h)))
-    del mailTime[index]
-
-    print('Init mailtime:',mailTime)
     m = int(uniform(1,59))#赋初值
     print('Init m：',m)
     #print(m)
@@ -28,20 +23,15 @@ def main():
         while True:
             #内部死循环是为了检测时间的
             now = datetime.now()
-            if now.hour == 1 and now.minute == 13:
-                mailTime = h.copy()
-                index = int(uniform(0, len(h)))
-                del mailTime[index]
-                print('Update mailtime:',mailTime)
-            if now.hour not in  mailTime:
+            if now.hour not in  h:
                 m = int(uniform(1, 59)) #只在非发信时段更新m值    
-            if now.hour in mailTime and now.minute == m:
+            if now.hour in h and now.minute == m:
                 break# 到达设定时间，结束内循环
             sleep(20)#检测周期是十秒
             print('Now Time：',now.hour,'：',now.minute,'：',now.second)
             print('Update m：',m)
         # 到点发信
-        mail(now.hour,mailTime)
+        mail(now.hour,h)
         sleep(60)
         # 等60秒，到下一分钟再开检测
 main()
